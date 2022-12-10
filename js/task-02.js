b2baff112f8fdac357f288495a0637da5fe270c4
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-10 13:34:30 +0200

    commit-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelectorAll("ingredients");
+console.log(listEl);
+
+ingredients.forEach((elem) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = elem;
+  console.log(itemEl);
+  itemEl.classList.add("item");
+});
+
+document.listEl.appendChild();
