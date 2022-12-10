77bafaa229bb0e656bf94b06f1205bb78907f37f
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-10 11:45:25 +0200

    commit-1

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+console.log("Number of categories:", itemEl.length);
+
+itemEl.forEach(function (elem) {
+  console.log("Category:", elem.firstElementChild.textContent);
+  console.log("Elements:", elem.lastElementChild.children.length);
+});
