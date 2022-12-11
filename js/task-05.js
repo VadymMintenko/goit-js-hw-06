d4d833612a344b78661b62f45998fafa4076cc15
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-11 22:08:16 +0200

    commit-5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector("#name-input");
+const textEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  textEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    textEl.textContent = "Anonymous";
+  }
+}
