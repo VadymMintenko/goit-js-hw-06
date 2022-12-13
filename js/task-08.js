864a8cfce8003fd0d576e30352784717a1ae367f
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-13 23:42:07 +0200

    commit-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+  if (!formData.email || !formData.password) {
+    alert("Заповніть всі поля");
+  } else {
+    form.reset();
+  }
+}
